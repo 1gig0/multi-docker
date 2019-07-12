10ac74fc4e14bce770301a1079916da90a44c095
gig-developer <giga.sagrishvili03@geolab.edu.ge>
2019-07-12 16:03:41 +0400

    Initial commit

---
+++ client/src/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import './App.css';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import OtherPage from './OtherPage';
+import Fib from './Fib';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <header>
+          <h1>Welcome to React</h1>
+          <p>
+            <Link to="/">Home</Link>
+          </p>
+          <p>
+            <Link to="/otherpage">Other Page</Link>
+          </p>
+        </header>
+        <div>
+          <Route exact path="/" component={Fib} />
+          <Route path="/otherpage" component={OtherPage} />
+        </div>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
